0755ea9afcdd779f5ce3d9539f63da7c75eb4cdd
radostBeZuma <romahasasha@yandex.ru>
2022-05-27 01:24:43 +0300

    добавил блок welcome-block

--- templates/index.php
+++ templates/index.php
@@ -8,20 +8,23 @@
 	<title>Главная страница</title>
 
 	<!-- ALL-page Css init -->
-	<link rel="stylesheet" href="/css/entry/variables.css">
-	<link rel="stylesheet" href="/css/entry/fonts.css">
-	<link rel="stylesheet" href="/css/entry/reset-style.css">
-	<link rel="stylesheet" href="/css/entry/components.css">
+		<link rel="stylesheet" href="/css/entry/variables.css">
+		<link rel="stylesheet" href="/css/entry/fonts.css">
+		<link rel="stylesheet" href="/css/entry/reset-style.css">
+		<link rel="stylesheet" href="/css/entry/components.css">
 	<!-- ALL-page Css init end -->
 
 	<!-- index-page css init -->
-	<link rel="stylesheet" href="/css/blocks/header.css">
+		<link rel="stylesheet" href="/css/blocks/header.css">
+		<link rel="stylesheet" href="/css/blocks/welcome-block.css">
 	<!-- index-page css init end -->
 </head>
 <body>
 	<div class="wrapper">
 		<? require_once("../blocks/header.php") ?>
-		
+		<div class="mt110">
+			<? require_once("../blocks/welcome-block.php") ?>
+		</div>
 	</div>
 </body>
 </html>
