63627c30d6fbcb4bdb6c00318de1b2d005468974
radostBeZuma <romahasasha@yandex.ru>
2022-05-27 01:04:48 +0300

    проект

---
+++ blocks/header.php
@@ -0,0 +1,30 @@
+<header class="header">
+
+	<div class="header__container-logo">
+		<p class="header__logo">SmartLibrary</p>
+	</div>
+
+	<nav class="header__menu">
+		<div class="header__container-link">
+			<a class="header__link link-primary link-primary_is-active" href="#">Главная</a>
+		</div>
+		<div class="header__container-link">
+			<a class="header__link link-primary" href="#">Каталог</a>
+		</div>
+		<div class="header__container-link">
+			<a class="header__link link-primary" href="#">Блог</a>
+		</div>
+		<div class="header__container-link">
+			<a class="header__link link-primary" href="#">Форум</a>
+		</div>
+	</nav>
+
+	<div class="header__container-login-n-registr">
+		<button class="header__btn-login">
+			<img class="header__img-login" src="/img/img-login.jpg" alt="Войти">
+			<p class="header__text-btn">Войти</p>
+		</button>
+
+		<button class="header__btn-regist btn-secondary">Регистрация</button>
+	</div>
+</header>
---
+++ templates/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Главная страница</title>
+
+	<!-- ALL-page Css init -->
+	<link rel="stylesheet" href="/css/entry/variables.css">
+	<link rel="stylesheet" href="/css/entry/fonts.css">
+	<link rel="stylesheet" href="/css/entry/reset-style.css">
+	<link rel="stylesheet" href="/css/entry/components.css">
+	<!-- ALL-page Css init end -->
+
+	<!-- index-page css init -->
+	<link rel="stylesheet" href="/css/blocks/header.css">
+	<!-- index-page css init end -->
+</head>
+<body>
+	<div class="wrapper">
+		<? require_once("../blocks/header.php") ?>
+		
+	</div>
+</body>
+</html>
