c66018c8811086e2df7b976a6981f31a323c9e9e
radostBeZuma <romahasasha@yandex.ru>
2022-05-31 14:12:00 +0300

    правки в блоках auth и registration

--- blocks/auth.php
+++ blocks/auth.php
@@ -1,20 +1,22 @@
 <section class="auth">
 	<div class="auth__blur-bg">
-		<div class="auth__container-img-n-form">
-			<button class="auth__close-btn" type="button">
-				<img class="auth__close-img" src="/img/close.png" alt="Иконка закрытия формы">
-			</button>
-			<div class="auth__box-img">
-				<img class="auth__img" src="/img/auth.png" alt="Иконка регистрации" >
-			</div>
-			<div class="auth__box-form">
-				<h4 class="auth__form-title">Авторизация</h4>
-				<div class="auth__container-inputs">
-					<input class="input-form auth__input" type="text" placeholder="Логин">
-					<input class="input-form auth__input" type="text" placeholder="Пароль">
+		<div class="auth__wrap">
+			<div class="auth__container-img-n-form">
+				<button class="auth__close-btn" type="button">
+					<img class="auth__close-img" src="/img/close.png" alt="Иконка закрытия формы">
+				</button>
+				<div class="auth__box-img">
+					<img class="auth__img" src="/img/auth.png" alt="Иконка регистрации" >
 				</div>
-				<div class="auth__container-btn">
-					<button class="btn-fourth auth__btn-sbt" type="button">Войти</button>
+				<div class="auth__box-form">
+					<h4 class="auth__form-title">Авторизация</h4>
+					<div class="auth__container-inputs">
+						<input class="input-form auth__input" type="text" placeholder="Логин">
+						<input class="input-form auth__input" type="text" placeholder="Пароль">
+					</div>
+					<div class="auth__container-btn">
+						<button class="btn-fourth auth__btn-sbt" type="button">Войти</button>
+					</div>
 				</div>
 			</div>
 		</div>
--- blocks/registration.php
+++ blocks/registration.php
@@ -1,22 +1,24 @@
-<section class="registration">
+<section class="registration _is-active">
 	<div class="registration__blur-bg">
-		<div class="registration__container-img-n-form">
-			<button class="registration__close-btn" type="button">
-				<img class="registration__close-img" src="/img/close.png" alt="Иконка закрытия формы">
-			</button>
-			<div class="registration__box-img">
-				<img class="registration__img" src="/img/regist.png" alt="Иконка регистрации" >
-			</div>
-			<div class="registration__box-form">
-				<h4 class="registration__form-title">Регистрация</h4>
-				<div class="registration__container-inputs">
-					<input class="input-form registration__input" type="text" placeholder="Имя">
-					<input class="input-form registration__input" type="text" placeholder="Логин">
-					<input class="input-form registration__input" type="text" placeholder="Пароль">
-					<input class="input-form registration__input" type="text" placeholder="Подтвердите пароль">
+		<div class="registration__wrap">
+			<div class="registration__container-img-n-form">
+				<button class="registration__close-btn" type="button">
+					<img class="registration__close-img" src="/img/close.png" alt="Иконка закрытия формы">
+				</button>
+				<div class="registration__box-img">
+					<img class="registration__img" src="/img/regist.png" alt="Иконка регистрации" >
 				</div>
-				<div class="registration__container-btn">
-					<button class="btn-secondary registration__btn-sbt" type="button">Регистрация</button>
+				<div class="registration__box-form">
+					<h4 class="registration__form-title">Регистрация</h4>
+					<div class="registration__container-inputs">
+						<input class="input-form registration__input" type="text" placeholder="Имя">
+						<input class="input-form registration__input" type="text" placeholder="Логин">
+						<input class="input-form registration__input" type="text" placeholder="Пароль">
+						<input class="input-form registration__input" type="text" placeholder="Подтвердите пароль">
+					</div>
+					<div class="registration__container-btn">
+						<button class="btn-secondary registration__btn-sbt" type="button">Регистрация</button>
+					</div>
 				</div>
 			</div>
 		</div>
