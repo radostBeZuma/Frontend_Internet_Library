5901701f89d1235646cbe920c65cd14bd35fa340
radostBeZuma <romahasasha@yandex.ru>
2023-05-29 17:41:50 +0300

    Добавление ссылки на детальную страницу

--- blocks/book.php
+++ blocks/book.php
@@ -1,6 +1,6 @@
 <section class="book">
 	<img class="book__img" src="/img/book.jpg" alt="Обложка книги">
-	<a class="book__detail-link" href="#">
+	<a class="book__detail-link" href="/detail/">
 		<p class="book__title">Последняя ночь у Извилистой реки</p>
 	</a>
 </section>
